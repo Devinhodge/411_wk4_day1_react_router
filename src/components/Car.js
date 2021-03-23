ed518d2b6412f8307fb3859814386bbbb08e747d
Devinhodge <d.hodge8891@yahoo.com>
2021-03-23 10:57:27 -0500

    completed

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,12 +1,53 @@
 import React from 'react'
 import cars from '../cars.json'
-// import material ui components here //
-// Container, Paper, Chip //
+import { Container, Paper, Chip, makeStyles } from '@material-ui/core';
 
-const Car = (props) => {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    alignItems: "center",
+
+    justifyContent: "center",
+    flexWrap: "wrap",
+    width: 300,
+    padding: 10,
+    "& > *": {
+      margin: theme.spacing(0.5),
+    },
+  },
+  chip: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    padding: 10,
+    gap: 10,
+  },
+}));
+  
+  const Car = (props) => {
+    const carId = props.match.params.id;
+    const carInfo = cars[carId];
+    const carClasses = useStyles();
+  
     return (
-        <h1>A specific car</h1>
-    )
-}
-
-export default Car
+      <div className={carClasses.root}>
+          <Container> 
+        <Paper elevation={3} display="flex" flex>
+          <h1>{carInfo.Name}</h1>
+          <div className={Chip}>
+            <Chip label={`MPG: ${carInfo.Miles_per_Gallon}`} />
+            <Chip label={`Cylinders: ${carInfo.Cylinders}`} />
+            <Chip label={`Displacement: ${carInfo.Displacement}`} />
+            <Chip label={`Horsepower: ${carInfo.Horsepower}`} />
+            <Chip label={`Weight: ${carInfo.Weight_in_lbs}`} />
+            <Chip label={`Acceleration: ${carInfo.Acceleration}`} />
+            <Chip label={`Year: ${carInfo.Year}`} />
+            <Chip label={`Origin: ${carInfo.Origin}`} />
+          </div>
+        </Paper>
+          </Container>
+      </div>
+    );
+  };
+  
+  export default Car;
